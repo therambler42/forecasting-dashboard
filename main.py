4d81810e8a20a7b3db4ba0c836d153563334a50b
Skunk Wooks <rambl@forecast.com>
2025-06-20 01:23:58 -0400

    Use minimal FastAPI setup for Render

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+async def root():
+    return {"message": "Hello from Render!"}
